06e825aa6d930c95f68aebb8a41633e1a18b01b4
nickbevers <nick.bevers9@gmail.com>
2022-10-08 20:33:48 +0200

    feat: add get route for all messages and messages per user with parameters

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+    if(req.query.user) {
+        res.json({
+            status: 'success',
+            message: `GETTING message for username ${req.query.user}`,
+            data: {
+                user: req.query.user
+            }
+        })
+    } else {
+        res.json({ 
+            status: 'success', 
+            message: 'GETTING messages', 
+            data: { 
+                messages: [
+                    {
+                        user: "John",
+                        message: "Hello"
+                    },
+                    {
+                        user: "Jane",
+                        message: "Hi"
+                    },
+                    {
+                        user: "Victor",
+                        message: "Hey Guys!"
+                    }
+                ]
+            }
+        });
+    }
 });
+
+
 
 module.exports = router;
