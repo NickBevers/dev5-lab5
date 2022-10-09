6d3d6f7aea1e9e2f367bf0c43f100e59f986076f
nickbevers <nick.bevers9@gmail.com>
2022-10-09 17:19:05 +0200

    test: testing new vercel settings

--- api/index.js
+++ api/index.js
@@ -1,44 +1,44 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const port = 3000;
 
-var indexRouter = require("../routes/index");
-var usersRouter = require("../routes/users");
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
 
 var app = express();
 
-app.use((req, res, next) => {
-    const origin = req.get("referer");
-    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-    if (isWhitelisted) {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader(
-            "Access-Control-Allow-Methods",
-            "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-        );
-        res.setHeader(
-            "Access-Control-Allow-Headers",
-            "X-Requested-With,Content-Type,Authorization"
-        );
-        res.setHeader("Access-Control-Allow-Credentials", true);
-    }
-    // Pass to next layer of middleware
-    if (req.method === "OPTIONS") res.sendStatus(200);
-    else next();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/api/v1/messages', indexRouter);
+app.use('/', (req, res) => { res.render('index', { title: 'Express' }); });
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-const setContext = (req, res, next) => {
-    if (!req.context) req.context = {};
-    next();
-};
-app.use(setContext);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.use("/api/v1/messages", indexRouter);
-app.use("/", (req, res) => {
-    res.send("This is an index page");
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
-app.use("/users", usersRouter);
 
 module.exports = app;
