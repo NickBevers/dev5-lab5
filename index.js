f2e4af35b63c26b688898d653bc6b82860905a33
nickbevers <nick.bevers9@gmail.com>
2022-10-09 21:20:41 +0200

    del: remove unneeded folders for vercel; del: remove all dependencies from the project except express

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const app = express();
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-// Setng up setings for the app
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 // Routers settings for the app
 app.use('/api/v1/messages', indexRouter);
 app.use('/', (req, res) => { res.sendFile(__dirname + '/public/index.html') });
 app.use('/users', usersRouter);
 
+app.listen(port, () =>
+    console.log(`Server running on ${port}, http://localhost:${port}`)
+);
